c64fd5953631daadb01a0c7e07198ef04aa23cc5
marco <marcoreda56@gmail.com>
2026-02-27 16:19:50 +0200

    fetch/header

---
+++ src/app/Compnents/header-component/header-component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-header-component',
+  imports: [RouterLink,CommonModule],
+  templateUrl: './header-component.html',
+  styleUrl: './header-component.css',
+})
+export class HeaderComponent {
+categories=["Electronics","Fasion","home"];
+}
