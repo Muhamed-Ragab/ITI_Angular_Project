527cb5da2561b653cc2cf95789eec44e148139ab
marco <marcoreda56@gmail.com>
2026-03-04 10:16:12 +0200

    change all suggestion

--- src/app/domains/home/home.component.ts
+++ src/app/domains/home/home.component.ts
@@ -1,7 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { RouterLink } from '@angular/router';
 import { AuthService } from '../../core/services/auth.service';
-import { CategoriesPage } from "./Pages/categories.page/categories.page";
 import { CategoryComponent } from "./Components/category-component/category-component";
 import { BestSellerComponent } from "./Components/best-seller-component/best-seller-component";
 
