99d6c8daa92ff0635034b19033d420cc675b3eed
marco <marcoreda56@gmail.com>
2026-03-09 21:32:17 +0200

    add user management page to admin dashboard

---
+++ src/app/domains/usermanagment/admin-service.ts
@@ -0,0 +1,33 @@
+import { inject, Injectable } from '@angular/core';
+import { Observable, map } from 'rxjs';
+import { ApiService } from '@app/core/services/api.service';
+
+@Injectable({ providedIn: 'root' })
+export class AdminService {
+  private api = inject(ApiService);
+  private readonly endpoint = '/users';
+
+  getUsers(params: any): Observable<any> {
+    return this.api.get<any>(this.endpoint, params);
+  }
+
+  getUserById(id: string): Observable<any> {
+    return this.api.get<any>(`${this.endpoint}/admin/${id}`);
+  }
+
+  updateUser(id: string, userData: any): Observable<any> {
+    return this.api.put(`${this.endpoint}/admin/${id}`, userData);
+  }
+
+  softDeleteUser(id: string): Observable<any> {
+    return this.api.delete(`${this.endpoint}/admin/${id}`);
+  }
+
+  toggleBan(id: string, isRestricted: boolean): Observable<any> {
+    return this.api.patch(`${this.endpoint}/admin/${id}/restriction`, { isRestricted });
+  }
+
+  grantLoyaltyPoints(id: string, points: number): Observable<any> {
+    return this.api.patch(`${this.endpoint}/admin/${id}/loyalty`, { points });
+  }
+}
